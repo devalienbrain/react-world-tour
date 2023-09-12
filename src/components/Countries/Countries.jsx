8e6efccd65d82386a1757d885aba7b726866b36e
Alien Brain <murad.dub@gmail.com>
2023-09-12 23:33:45 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import Country from "../Country/Country";
+import "./countries.css";
+
+const Countries = () => {
+  // To load api data: declare state
+  const [countries, setCountries] = useState([]);
+
+  // Hook: to fetch data and set state
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h3>Countries: Total {countries.length}</h3>
+
+      <div className="countries-container">
+        {countries.map((country) => (
+          <Country key={country.cca2} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Countries;
